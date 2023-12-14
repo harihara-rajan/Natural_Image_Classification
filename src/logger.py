a56e28c8e59ef424f04f8dca21f9ea8d4b1e40e4
Hari <harihararajanrm.cms@gmail.com>
2023-12-14 20:09:54 +0100

    file for logging and logging config implemented

---
+++ src/logger.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import logging
+
+
+log_filename = "runninglogs.log"
+log_file_path = os.path.join(os.getcwd(), "logs", log_filename)
+
+# create directory for the logfile 
+os.makedirs(os.path.dirname(log_file_path))
+with open(log_file_path, 'w') as file:
+    pass
+
+log_format = '[%(asctime)s] : %(levelname)s : %(module)s : %(message)s]' # format
+
+logging.basicConfig(
+    level = logging.INFO,
+    format= log_format, 
+    handlers=[
+        logging.FileHandler(log_file_path), 
+        logging.StreamHandler(sys.stdout)
+    ]
+)
