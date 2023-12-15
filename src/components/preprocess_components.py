a7fdec2e8df60cd42a9ffbce944df6f2b8fd88b8
Hari <harihararajanrm.cms@gmail.com>
2023-12-15 14:36:49 +0100

    documentations added for the preprocessing components

--- src/components/preprocess_components.py
+++ src/components/preprocess_components.py
@@ -3,11 +3,28 @@ from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader, SubsetRandomSampler
 from sklearn.model_selection import train_test_split
 from src.entity.entity_config import ConfigPreprocess
+from src.logger import logging
 class PreprocessComponent:
+    """
+    This class consists of several components as methods for performing 
+    several preprocessing operations like
+    0. Load the data
+    1. Augmentation
+        - resizing the image to the required dimensions
+        - Flipping the image 
+        - Normalizing the image        
+    2. Splitting the data into train-test split
+
+    """
     def __init__(self, config_preprocess:ConfigPreprocess):
         self.config = config_preprocess
     
     def perform_augmentation(self):
+        """
+        This function is used to perform augmentations and normalising the training data,
+        while the validation data were only normalised.
+
+        """
         if self.config.Augmentation:
             train_transform = transforms.Compose([
                 transforms.RandomResizedCrop(self.config.Image_Size[0]),
@@ -28,8 +45,15 @@ class PreprocessComponent:
         return [train_transform, val_transform]
 
     def split_train_test(self, t_data, v_data):
+        """
+        t_data: augmentations performed on all the data 
+        v_data: no augmentations were performed 
+        len(t_data) == len(v_data) == total data 
+        however, the augmentation were different for t_data and v_data 
+        """
+
         total_indices= list(range(len(t_data)))
-        t_indices, v_indices = train_test_split(total_indices, test_size=0.3)
+        t_indices, v_indices = train_test_split(total_indices, test_size=0.3) #TODO: move test size to params.yaml
         t_sampler = SubsetRandomSampler(t_indices)
         v_sampler = SubsetRandomSampler(v_indices)
         t_loader = DataLoader(dataset=t_data,
@@ -42,6 +66,14 @@ class PreprocessComponent:
         return t_loader, v_loader
     
     def load_data(self):
+        """
+        this function  creates a transformations required 
+        before loading the dataset, and the transformations 
+        are then passed to ImageFolder function to perform
+        certain augmentation operations on the train and 
+        val data 
+        """
+
         transform_list = self.perform_augmentation()
  
         train_data = ImageFolder(self.config.data_folder, 
@@ -53,12 +85,6 @@ class PreprocessComponent:
     def preprocess_data(self):
         """
         preprocessing steps:
-        0. Load the data
-        1. Augmentation
-            - resizing the image to the required dimensions
-            - Flipping the image 
-            - Normalizing the image        
-        2. Splitting the data into train-test split
         """
         t_data_transform, v_data_transform = self.load_data()
         t_loader, v_loader = self.split_train_test(t_data_transform, v_data_transform)
--- src/utils/common.py
+++ src/utils/common.py
@@ -1,6 +1,7 @@
 import yaml 
 from box import ConfigBox
 from pathlib import Path
+from src.logger import logging
 def read_yaml(path:Path):
     """
     path: Path to Yaml file to be read
@@ -10,4 +11,5 @@ def read_yaml(path:Path):
     """
     with open(path, 'r') as yaml_file:
         content = yaml.safe_load(yaml_file)
+        logging.info(f'Read yaml file: {path}: Success')
     return ConfigBox(content)
